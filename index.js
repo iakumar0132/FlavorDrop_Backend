4e1e4fb2abd053d830c2f17f671527402153eb9e
Himanshu Kumar <hkumar0132@gmail.com>
2023-08-09 22:32:33 +0530

    fix cors issue

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
 
 const express = require('express')
-const cors = require('cors');
 require('dotenv').config();
 
 
 global.foodData = require('./db')(function call(err, data, CatData) {
-  //console.log("Data",data)
   if(err) console.log(err);
   global.foodData = data;
   global.foodCategory = CatData;
 })
 const PORT = process.env.PORT || 7000;
-const app = express()
-app.use(cors())
+const app = express();
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", `http://localhost:${PORT}`);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header('Access-Control-Allow-Origin', 'https://flavordrop.netlify.app/');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:6019');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 app.use(express.json())
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('All Good!')
 })
 
 app.use('/api/auth', require('./Routes/Auth'));
